a91b85de497d2174e2a613642016b77d68e0301f
liumapp <liumapp.com@gmail.com>
2020-06-11 15:05:00 +0800

    add simple net demo module

---
+++ simple-net-demo/src/main/java/com/liumapp/simplenet/demo/App.java
@@ -0,0 +1,13 @@
+package com.liumapp.simplenet.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
